3028452456a43e065b241e4d9831b069f237158c
xiehui <xiehui0902@gmail.com>
2017-04-23 23:04:24 +0800

    something
    something

--- app/src/main/java/com/learnrecord/People.kt
+++ app/src/main/java/com/learnrecord/People.kt
@@ -2,8 +2,39 @@ package com.learnrecord
 
 /**
  *Created by Code4Android on 2017/4/22.
+ * abstract //抽象类标示
+ * final  //标示类不可继承，默认属性
+ * enum  //标示类为枚举
+ * open  //类可继承，类默认是final的
+ * annotation  //注解类
+ *
+ *
+ * private //仅在同一个文件中可见
+ * protected //同一个文件中或子类可见
+ * public //所有调用的地方都可见
+ * internal //同一个模块中可见
+ * constructor:构造函数
+ * constructor无修饰符(如：private)时，constructor可以省略：
+ * 当是无参构造函数时，整个构造函数部分也可以省略，省略的构造函数默认是public的
+ * 由于primary constructor不能包含任何代码，因此使用 init 代码块对其初始化,
+ * 同时可以在初始化代码块中使用构造函数的参数
  */
-open class People constructor(var id:String, override var name: String):Iwant1{
+//primary constructor
+open class People  constructor(var id: String, override var name: String) : Iwant1 {
+    //可以类中初始化属性：
+    var customName = name.toUpperCase() //初始化属性
+    //使用constructor前缀声明，且必须调用primary constructor，使用this关键字
+    constructor(id:String,name:String, age:Int):this(id,name){
+
+    }
+    init {
+        print("初始化操作，可使用constructor参数")
+    }
+    //需要open修饰，子类才可以
+    open fun study(){
+        print("study")
+    }
+    //override修饰的方法，默认是可以被继承的。若希望不被继承，可以使用 final 关键词修饰
     override fun drink() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
--- app/src/main/java/com/learnrecord/Student.kt
+++ app/src/main/java/com/learnrecord/Student.kt
@@ -3,11 +3,17 @@ package com.learnrecord
 /**
  *Created by Code4Android on 2017/4/22.
  * Number类型：Float,Double,Int,Byte,Short,Char
+ * 当接口未实现方法时，默认为父类的方法,不需要调用所继承的方法
+ * Kotlin中的抽象类允许有abstract修饰的成员方法，非抽象类不允许有抽象方法；
+ * 抽象类默认是可被继承的，接口是特殊的抽象类，允许有抽象方法
  */
+
 class Student(id: String, name: String) : People(id, name) {
     var test:Number=3
 
-
+    override fun study() {
+        super.study()
+    }
     fun beat(){
         println("Int max value:"+Int.MAX_VALUE)
         println("Int min value:"+Int.MIN_VALUE)
