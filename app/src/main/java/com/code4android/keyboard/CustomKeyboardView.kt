74fe7d545f1a4e3adc406e43caffb50b1c385826
xiehui <xiehui0902@gmail.com>
2017-09-07 18:09:08 +0800

    keyboard
    keyboard

--- app/src/main/java/com/code4android/keyboard/CustomKeyboardView.kt
+++ app/src/main/java/com/code4android/keyboard/CustomKeyboardView.kt
@@ -2,11 +2,9 @@ package com.code4android.keyboard
 
 import android.content.Context
 import android.graphics.*
-import android.graphics.drawable.ColorDrawable
 import android.inputmethodservice.Keyboard
 import android.inputmethodservice.KeyboardView
 import android.util.AttributeSet
-import com.code4android.kotlinforandroid.R
 
 /**
  *Created by Code4Android on 2017/9/5.
@@ -30,16 +28,17 @@ class CustomKeyboardView : KeyboardView {
         if (keys != null) {
             for (key in keys) {
                 //可以自定义自己的绘制（例如某个按钮绘制背景图片和文字，亦或者更改某个按钮颜色等）
-                if (key.codes[0] == Keyboard.KEYCODE_CANCEL) {
-/*                    drawBackground(R.color.btnnormal, canvas, key)
-                    drawText(canvas, key)*/
+                if (key.codes[0] == -111) {//过滤指定某个键自定义绘制
+                    //绘制后，原来xml中的keyLabel以及keyIcon会被覆盖,如需显示文字需要自己重新绘制，要后绘制文字，否则文字不显示
+/*                    drawBackground(R.drawable.bg_keyboardview1, canvas, key)
+                    drawTextOrIcon(canvas, key)*/
                 }
             }
         }
     }
 
-    //绘制文字
-    fun drawText(canvas: Canvas, key: Keyboard.Key) {
+    //绘制文字或图标
+    fun drawTextOrIcon(canvas: Canvas, key: Keyboard.Key) {
         var bounds = Rect()
         var paint = Paint()
         paint.color = Color.WHITE
@@ -48,6 +47,12 @@ class CustomKeyboardView : KeyboardView {
         paint.typeface = Typeface.DEFAULT
         if (key.label != null) {
             var label = key.label.toString()
+            //为了将字体大小与默认绘制的Label字体大小相同，需要反射获取默认大小。然后在此处设置文字大小
+            //还有一种取巧的方法在布局文件keyboardview中设置keyTextSize，labelTextSize
+            var field = KeyboardView::class.java.getDeclaredField("mLabelTextSize")
+            field.isAccessible = true
+            var labelTextSize = field.get(this) as Int
+            paint.textSize = labelTextSize.toFloat()
             paint.getTextBounds(label, 0, label.length, bounds)
             canvas.drawText(label, (key.x + key.width / 2).toFloat(), (key.y + key.height / 2 + bounds.height() / 2).toFloat(), paint)
         } else if (key.icon != null) {
@@ -57,10 +62,14 @@ class CustomKeyboardView : KeyboardView {
     }
 
     //绘制背景
-    fun drawBackground(color: Int, canvas: Canvas, key: Keyboard.Key) {
-        var colorDraw = ColorDrawable(color)
-        colorDraw.bounds = Rect(key.x, key.y, key.x + key.width, key.height + key.y)
-        colorDraw.draw(canvas)
+    fun drawBackground(drawableId: Int, canvas: Canvas, key: Keyboard.Key) {
+        var drawable = resources.getDrawable(drawableId)
+        var drawableState: IntArray = key.currentDrawableState
+        if (key.codes[0] != 0) {
+            drawable.setState(drawableState)
+        }
+        drawable.bounds = Rect(key.x, key.y, key.x + key.width, key.height + key.y)
+        drawable.draw(canvas)
     }
 
 }
--- app/src/main/java/com/code4android/keyboard/KeyBoardUtil.kt
+++ app/src/main/java/com/code4android/keyboard/KeyBoardUtil.kt
@@ -6,38 +6,72 @@ import android.inputmethodservice.KeyboardView
 import android.os.Build
 import android.text.Editable
 import android.text.InputType
+import android.text.TextWatcher
 import android.util.Log
+import android.view.Gravity
+import android.view.KeyEvent
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.EditText
+import android.widget.FrameLayout
 import com.code4android.kotlinforandroid.R
 import org.jetbrains.anko.find
 import org.jetbrains.anko.inputMethodManager
 import java.lang.RuntimeException
 import java.lang.reflect.Method
+import java.util.*
 
 /**
  *Created by Code4Android on 2017/9/6.
  */
 class KeyBoardUtil {
+
     var mActivity: Activity
     var mIsRandom: Boolean = false
     var mKeyboard: Keyboard
-    var mKeyBoardView: CustomKeyboardView
+    lateinit var mKeyBoardViewContainer: View
+    lateinit var mKeyBoardView: CustomKeyboardView
     var mEditText: EditText? = null
     val TAG = "Keyboard"
     var mOnOkClick: OnOkClick? = null
     var mOnCancelClick: OnCancelClick? = null
+    var mIsDouble = false
 
-    constructor(activity: Activity) : this(activity, false)
-    constructor(activity: Activity, isRandom: Boolean) {
+    constructor(activity: Activity) : this(activity, true, false)
+    constructor(activity: Activity, isRandom: Boolean, isDouble: Boolean) {
         mActivity = activity
         mIsRandom = isRandom
+        mIsDouble = true
         mKeyboard = Keyboard(mActivity, R.xml.keyboard)
-        mKeyBoardView = mActivity.find(R.id.keyboard_view)
+        addViewToRoot()
+
+    }
+
+
+    private fun addViewToRoot() {
+        mKeyBoardViewContainer = mActivity.layoutInflater.inflate(R.layout.keyboardview, null)
+        //var frameLayout: FrameLayout = mActivity.window.decorView as FrameLayout//不要直接往DecorView(状态栏，内容，导航栏)中addView，如使用这个则最后显示布局不全（一部分内容在导航栏区域）
+        var frameLayout: FrameLayout = mActivity.window.decorView.find(android.R.id.content)
+        var lp = FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.WRAP_CONTENT)
+        lp.gravity = Gravity.BOTTOM
+        frameLayout.addView(mKeyBoardViewContainer, lp)
+        mKeyBoardView = mKeyBoardViewContainer.find(R.id.keyboard_view)
+            if (keyCode == KeyEvent.KEYCODE_BACK) {
+                if (mKeyBoardView.visibility == View.VISIBLE) {
+                    mKeyBoardView.visibility = View.GONE
+                    startAnimation(false)
+                    true
+                }
+                false
+            }
+            false
+        }
     }
 
     fun attachTo(editText: EditText) {
-        if (!(editText is EditText)) throw RuntimeException("Type error")
+        if (editText !is EditText) throw RuntimeException("Type error")
+        if (mEditText != null && mEditText == editText) return
         mEditText = editText
         Log.e(TAG, "attachTo")
         onFoucsChange()
@@ -50,13 +84,24 @@ class KeyBoardUtil {
             Log.e(TAG, "onFoucsChange:$hasFocus" + v)
             if (hasFocus && mKeyBoardView.visibility != View.VISIBLE) {
                 mKeyBoardView.visibility = View.VISIBLE
+                startAnimation(true)
             } else if (!hasFocus && mKeyBoardView.visibility == View.VISIBLE) {
                 mKeyBoardView.visibility = View.GONE
+                startAnimation(false)
             }
         }
+
+        mEditText!!.setOnClickListener {
+            Log.e(TAG, "setOnClickListener")
+            if (mKeyBoardView.visibility == View.GONE) {
+                mKeyBoardView.visibility = View.VISIBLE
+                startAnimation(true)
+            }
+        }
+
     }
 
-    fun hideSystemSoftKeyboard() {
+    private fun hideSystemSoftKeyboard() {
         if (Build.VERSION.SDK_INT > 10) {
             var clazz = EditText::class.java
             var setShowSoftInputOnFocus: Method? = null
@@ -80,14 +125,39 @@ class KeyBoardUtil {
 
     private fun showSoftKeyboard() {
         if (mIsRandom) {
-
+            generateRandomKey()
         } else {
             mKeyBoardView.keyboard = mKeyboard
         }
         mKeyBoardView.isEnabled = true
-        mKeyBoardView.isPreviewEnabled = false
+        mKeyBoardView.isPreviewEnabled = true
         mKeyBoardView.visibility = View.VISIBLE
         mKeyBoardView.setOnKeyboardActionListener(mOnKeyboardActionListener())
+    }
+
+    private fun generateRandomKey() {
+        var keys = mKeyboard.keys
+        var numberKeys = mutableListOf<Keyboard.Key>()
+        //保存数字
+        var nums = mutableListOf<Int>()
+        //0的ASCII码是48,之后顺序加1
+        for (key in keys) {
+            if (key.label != null && "0123456789".contains(key.label)) {
+                nums.add(Integer.parseInt(key.label.toString()))
+                numberKeys.add(key)
+            }
+        }
+        var random = Random()
+        var changeKey = 0//更改numberKeys对应的数值
+        while (nums.size > 0) {
+            var size = nums.size
+            var randomNum = nums[random.nextInt(size)]
+            var key = numberKeys[changeKey++]
+            key.codes[0] = 48 + randomNum
+            key.label = randomNum.toString()
+            nums.remove(randomNum)
+        }
+        mKeyBoardView.keyboard = mKeyboard
     }
 
 
@@ -98,6 +168,8 @@ class KeyBoardUtil {
 
         override fun onPress(primaryCode: Int) {
             Log.e(TAG, "onPress")
+            //指定隐藏删除不显示预览
+            mKeyBoardView.isPreviewEnabled = !(primaryCode == Keyboard.KEYCODE_CANCEL || primaryCode == Keyboard.KEYCODE_DELETE)
         }
 
         override fun onRelease(primaryCode: Int) {
@@ -122,6 +194,7 @@ class KeyBoardUtil {
 
             mEditText?.let {
                 var editable: Editable = it.text
+                var textString = editable.toString()
                 var start = it.selectionStart
                 when (primaryCode) {
                     Keyboard.KEYCODE_DELETE -> {
@@ -139,18 +212,28 @@ class KeyBoardUtil {
                             it.onCancelClick()
                         }
                     }
-                    Keyboard.KEYCODE_DONE -> {
-                        hideSoftKeyboard()
-                        mOnOkClick?.let {
-                            it.onOkClick()
-                        }
-                    }
                     else -> {
-                        editable.insert(start, Character.toString(primaryCode.toChar()))
+                        //   由于promaryCode是用的ASCII码，则直接转换字符即可
+                        if ((primaryCode != 46 && !mIsDouble) || mIsDouble && primaryCode != 46) {
+                            editable.insert(start, Character.toString(primaryCode.toChar()))
+                        } else {
+                            if (mIsDouble && primaryCode == 46) {
+                                if ("" == textString) {
+                                    editable.insert(start, "0.")
+                                } else if (!textString.contains(".")) {
+                                    if (start == 0) {
+                                        editable.insert(start, "0.")
+                                    } else {
+                                        editable.insert(start, ".")
+                                    }
+                                } else {
+                                }
+                            } else {
+                            }
+                        }
                     }
                 }
             }
-
         }
 
         override fun onText(text: CharSequence?) {
@@ -169,8 +252,20 @@ class KeyBoardUtil {
     fun hideSoftKeyboard() {
         var visibility = mKeyBoardView.visibility
         if (visibility == View.VISIBLE) {
+            startAnimation(false)
             mKeyBoardView.visibility = View.GONE
         }
+    }
+
+    fun startAnimation(isIn: Boolean) {
+        Log.e(TAG, "startAnimation")
+        var anim: Animation
+        if (isIn) {
+            anim = AnimationUtils.loadAnimation(mActivity, R.anim.anim_bottom_in)
+        } else {
+            anim = AnimationUtils.loadAnimation(mActivity, R.anim.anim_bottom_out)
+        }
+        mKeyBoardViewContainer.startAnimation(anim)
     }
 }
 
